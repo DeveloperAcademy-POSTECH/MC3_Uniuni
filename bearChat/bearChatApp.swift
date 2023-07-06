e64446da291826198be9f975d850513416fe9781
HansolWorld <hs9269@gmail.com>
2023-07-06 16:50:20 +0900

    chore: init setting

---
+++ bearChat/bearChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  bearChatApp.swift
+//  bearChat
+//
+//  Created by apple on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct bearChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
