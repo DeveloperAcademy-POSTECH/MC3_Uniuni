c79d53b9b020992b0c8960cf1d03a5ef7f797cf2
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2023-07-14 13:59:20 +0900

    chore: move files to improve folder structure
    The added views are temporary files for recognizing the structure

---
+++ BeerChat/Views/Components/CustomButton.swift
@@ -0,0 +1,20 @@
+//
+//  CustomButton.swift
+//  BeerChat
+//
+//  Created by Jun on 2023/07/14.
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Button_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomButton()
+    }
+}
