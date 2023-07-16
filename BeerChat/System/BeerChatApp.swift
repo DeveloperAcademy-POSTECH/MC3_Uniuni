c6743eacd08ba4c0ddef8836c286b460b58438aa
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2023-07-16 19:44:04 +0900

    fix: modify for temporarily login
    modify for temporarily login

--- BeerChat/System/BeerChatApp.swift
+++ BeerChat/System/BeerChatApp.swift
@@ -14,6 +14,12 @@ struct BeerChatApp: App {
     // Firebase initialization following SwiftUI Life Style. to be revised soon.
     init() {
         FirebaseApp.configure()
+        UserManager.shared.fetchCurrentUser(userId: "iyNMs7XySOgBVmxNOS0lvkUlt6m2", completion: { user in
+            if let userid = user?.userId {
+                PageManager.shared.currentPage = .main
+            }
+        })
+        /*
         if let uid = Auth.auth().currentUser?.uid {
             UserManager.shared.fetchCurrentUser(userId: uid, completion: { user in
                 if let userid = user?.userId {
@@ -21,7 +27,7 @@ struct BeerChatApp: App {
                     PageManager.shared.currentPage = .main
                 }
             })
-        }
+        }*/
     }
 
     var body: some Scene {
