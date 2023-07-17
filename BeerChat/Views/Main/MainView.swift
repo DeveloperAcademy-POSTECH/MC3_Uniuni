60afd3418735b474ccd6a7d1b12ef877bf8d417c
HansolWorld <hs9269@gmail.com>
2023-07-17 13:45:16 +0900

    feat: Add pageIndex for move page after matching

--- BeerChat/Views/Main/MainView.swift
+++ BeerChat/Views/Main/MainView.swift
@@ -8,40 +8,32 @@
 import SwiftUI
 
 struct MainView: View {
-    @State var isMatching: Bool = false
-    @State var matchingUser: User?
+    @State var pageIndex: Int = 0
     @StateObject var firestoreManager: FirestoreManager = FirestoreManager()
     
     var body: some View {
         NavigationStack {
-            TabView {
-                MatchingView(isMatching: $isMatching, matchingUser: $matchingUser)
+            TabView(selection: $pageIndex) {
+                MatchingView(pageIndex: $pageIndex)
+                    .environmentObject(firestoreManager)
                     .tabItem {
                         Image(systemName: "house.fill")
                         Text("Main")
                     }
-                    .onChange(of: isMatching) { newValue in
-                        if !newValue {
-                            matchingUser = nil
-                        }
-                    }
-                    .fullScreenCover(isPresented: $isMatching) {
-                        if let matchingUser = self.matchingUser {
-                            MatchingProfileView(user: matchingUser, isPresentedSheet: $isMatching)
-                                .environmentObject(firestoreManager)
-                        }
-                    }
+                    .tag(0)
                 ChatListView()
                     .environmentObject(firestoreManager)
                     .tabItem {
                         Image(systemName: "message.fill")
                         Text("Chat")
                     }
+                    .tag(1)
                 ProfileView()
                     .tabItem {
                         Image(systemName: "person.fill")
                         Text("My")
                     }
+                    .tag(2)
             }
             .onAppear {
                 UITabBar.appearance().backgroundColor = UIColor.white.withAlphaComponent(0.75)
--- BeerChat/Views/Main/Matching/MatchingProfileView.swift
+++ BeerChat/Views/Main/Matching/MatchingProfileView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MatchingProfileView: View {
     let user: User
     @Binding var isPresentedSheet: Bool
+    @Binding var pageIndex: Int
     @EnvironmentObject var firestoreManager: FirestoreManager
     var body: some View {
         VStack(alignment: .leading) {
@@ -53,6 +54,8 @@ struct MatchingProfileView: View {
                     guard let currentUserId = UserManager.shared.currentUser?.userId,
                           let partneruserId = user.userId else { return }
                     firestoreManager.addChatRoom(userId: currentUserId, partnerId: partneruserId)
+                    pageIndex = 1
+                    isPresentedSheet = false
                 } label: {
                     Text("수락하기")
                         .frame(width: 110, height: 30)
@@ -67,6 +70,6 @@ struct MatchingProfileView: View {
 
 struct MatchingProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        MatchingProfileView(user: User(affiliation: "애플아카데미", major: "개발", yearOfAdmission: 2018), isPresentedSheet: .constant(true))
+        MatchingProfileView(user: User(affiliation: "애플아카데미", major: "개발", yearOfAdmission: 2018), isPresentedSheet: .constant(true), pageIndex: .constant(0))
     }
 }
