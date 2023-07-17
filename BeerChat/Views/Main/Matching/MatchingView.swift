effb97fd2dbae1c0f60ff5f02de3d2b3c21080d2
HansolWorld <hs9269@gmail.com>
2023-07-17 10:36:32 +0900

    View: Add Matchingview

--- BeerChat/Views/Main/Matching/MatchingView.swift
+++ BeerChat/Views/Main/Matching/MatchingView.swift
@@ -10,13 +10,24 @@ import SwiftUI
 struct MatchingView: View {
     @State var seletedKeywords: Set<String> = []
     @State var lastKeyword: String = ""
+    var keywordCount: Int {
+        seletedKeywords.count
+    }
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text("질문할\n키워드 선택")
                 .font(.largeTitle.weight(.bold))
             Spacer()
             KeywordListView(seletedKewords: $seletedKeywords, lastkeyword: $lastKeyword)
-            Button(action: {} ) {
+                .onChange(of: keywordCount) { _ in
+                    if keywordCount > 2 {
+                        seletedKeywords.remove(lastKeyword)
+                    }
+                }
+            Button(action: {
+                
+            } ) {
                 Text("확인")
                     .font(.title2.weight(.bold))
                     .foregroundColor(Color.white)
