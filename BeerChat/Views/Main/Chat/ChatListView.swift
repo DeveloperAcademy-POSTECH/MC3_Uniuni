1123329b9fc9128bf996288cf39f85ac889ba7b4
HansolWorld <hs9269@gmail.com>
2023-07-17 17:25:20 +0900

    fix: fix typo

--- BeerChat/Views/Main/Chat/ChatListView.swift
+++ BeerChat/Views/Main/Chat/ChatListView.swift
@@ -11,7 +11,7 @@ import Firebase
 struct ChatListView: View {
     @EnvironmentObject var firestoreManager: FirestoreManager
     @State private var isLogin: Bool = false
-    @Binding var chatroomId: String
+    @Binding var chatRoomId: String
     @State var userEmail = ""
     @State var userId = ""
     @State var searchingText = ""
@@ -20,7 +20,7 @@ struct ChatListView: View {
         List {
             ForEach(firestoreManager.chatRooms, id: \.self.roomId) { chatRoom in
                 NavigationLink(destination: ChatView(chatRoom: chatRoom, userId: userId)
-                    .environmentObject(firestoreManager), isActive: isActiveBinding(chatRoom.roomId ?? "")) {
+                    .environmentObject(firestoreManager)) {
                         if let recentMessage = chatRoom.recentMessage {
                             HStack {
                                 Image(systemName: "person.fill")
@@ -72,22 +72,12 @@ struct ChatListView: View {
             }
         }
     }
-    
-    private func isActiveBinding(_ user: String) -> Binding<Bool> {
-        .init {
-            user == chatroomId
-        } set: { isActive in
-            if isActive {
-                chatroomId = user
-            }
-        }
-    }
 }
 
 struct ChatListView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
-            ChatListView(chatroomId: .constant(""))
+            ChatListView(chatRoomId: .constant(""))
                 .environmentObject(FirestoreManager())
         }
     }
